505a6291391080c7bc1142364dcbd36e5ddb310f
kba977 <kba977@163.com>
2015-04-15 18:40:40 +0800

    Integration with the Python Shell

--- flasky.py
+++ flasky.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
@@ -88,6 +88,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manage.add_command("shell", Shell(make_context=make_shell_context))
 
 
 if __name__ == '__main__':
