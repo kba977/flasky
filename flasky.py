ec281d47923d2141711cb23f048a341bf6dcb5c3
kba977 <kba977@163.com>
2015-04-15 20:30:44 +0800

    异步邮件服务 测试成功

--- flasky.py
+++ flasky.py
@@ -13,6 +13,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 from datetime import datetime
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -42,7 +43,6 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
-
 
 
 class NameForm(Form):
@@ -104,20 +104,28 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-# @app.errorhandler(500)
-# def internal_server_error(e):
-#     return render_template('500.html'), 500
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 if __name__ == '__main__':
