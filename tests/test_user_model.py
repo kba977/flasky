258c77e2894fc6c7f2dc5a10b98884b67ff4951f
kba977 <kba977@163.com>
2015-04-17 19:28:28 +0800

    C9a 用户身份和权限设置 正常工作

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User
+from app.models import User, AnonymousUser, Role, Permission
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -18,8 +18,17 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
-
     def test_password_salts_are_random(self):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
