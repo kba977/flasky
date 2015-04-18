1155a53c558261995be0f9305b21ba3a989e95c8
kba977 <kba977@163.com>
2015-04-18 19:01:08 +0800

    C10c User avatars

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+import hashlib
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from . import login_manager, db
-from flask import current_app
+from flask import current_app, request
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
@@ -102,6 +103,16 @@ class User(UserMixin, db.Model):
         """
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
