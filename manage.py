62e707e9bbebb17a4211fd5246d95c831f063636
kba977 <kba977@163.com>
2015-04-19 17:55:42 +0800

    followers 数据库关系的实现 完成

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import User, Role, Post, Permission
+from app.models import User, Follow, Role, Post, Permission
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -12,7 +12,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Post=Post, Permission=Permission)
+    return dict(app=app, db=db, User=User,Follow=Follow, Role=Role, Post=Post, Permission=Permission)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
